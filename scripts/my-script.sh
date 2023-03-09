09d535c24eebeea7ae898e6bf6ab9d386c391b7d
HollyRafiqueKCL <holly.rafique@kcl.ac.uk>
2023-03-09 14:18:34 +0000

    not tested: updated script to make empty dirs and README.md

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+#empty dirs in repo
+mkdir -p analysis docs data
+
+#adding READMEs to each dir
+for my_directory in scripts analysis docs data; do
+	touch ${my_directory}/README.md
+	echo "# ${my_directory}" >> ${my_directory}/README.md
+done
 
